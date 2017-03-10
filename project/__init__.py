6340a58fa63ee3fc9ce9f00ea9fb7680b096fc5e
Aaron M <aaron.m.manley@gmail.com>
2017-03-10 14:13:43 -0800

    seeded db, rm unnecc file

--- project/__init__.py
+++ project/__init__.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'postgres://localhost/warbler-api-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['TEMPLATES_AUTO_RELOAD'] = True
