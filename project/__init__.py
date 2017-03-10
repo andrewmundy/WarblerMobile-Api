703c94ecbb02b4c8c9dfd5c022b2160883fe0a4f
Aaron M <aaron.m.manley@gmail.com>
2017-03-10 11:51:38 -0800

    initial commit - reconfigured to use API.users, spin up server

--- project/__init__.py
+++ project/__init__.py
@@ -1,26 +1,45 @@
-import os
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
+from flask_modus import Modus
 from flask_bcrypt import Bcrypt
-from datetime import timedelta
-from flask_cors import CORS
+from flask_login import LoginManager, current_user
+import os
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
 
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or "postgres://localhost/full-stack-auth"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'postgres://localhost/warbler-api-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_AUTH_URL_RULE'] = '/api/users/auth'
-app.config['SECRET_KEY'] = 'shhhh'
-app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=3600)
-app.config['JWT_DEFAULT_REALM'] = "Login Required"
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or "Warbler Warbler yay! (I'M A SCRET)"
 
-from project.messages.views import messages_api
+modus = Modus(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+db = SQLAlchemy(app)
+
+
+
+# from project.messages.views import messages_blueprint 
+from project.models import User 
+# from project.messages.models import Message
 from project.users.views import users_api
 
 app.register_blueprint(users_api.blueprint, url_prefix='/api')
-app.register_blueprint(messages_api.blueprint, url_prefix='/api/users/<int:user_id>')
+# app.register_blueprint(messages_blueprint, url_prefix='/users/<int:id>/messages')
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
--- project/users/views.py
+++ project/users/views.py
@@ -1,14 +1,12 @@
-from flask import Blueprint, abort, request
-from flask_restful import Api, Resource, reqparse, marshal_with, fields
+from flask import redirect, render_template, request, url_for, Blueprint, flash, abort
 from project.models import User
-from project import db,bcrypt
-import jwt
+from project import db, bcrypt
 from sqlalchemy.exc import IntegrityError
+from flask_login import login_user, logout_user, current_user, login_required
 from functools import wraps
-from jwt.exceptions import DecodeError
+from flask_restful import Api, Resource, reqparse, marshal_with, fields
 
-users_api = Api(Blueprint('users_api', __name__))
-
+# cped over code
 def authenticate(username, password):
     user = User.query.filter(User.username == username).first()
     if bcrypt.check_password_hash(user.password, password):
@@ -19,7 +17,7 @@ def jwt_required(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
         if request.headers.get('authorization'):
-            split_token = request.headers.get('token').split(' ')[2]
+            split_token = request.headers.get('authorization').split(' ')[1]
         try:
             token = jwt.decode(split_token, 'secret', algorithm='HS256')
             if token:
@@ -34,8 +32,9 @@ def jwt_required(fn):
 def ensure_correct_user(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        if request.headers.get('token'):
-            split_token = request.headers.get('token').split(' ')[2]
+
+        if request.headers.get('authorization'):
+            split_token = request.headers.get('authorization').split(' ')[2]
         try:
             token = jwt.decode(split_token, 'secret', algorithm='HS256')
             if kwargs.get('id') == token.get('id'):
@@ -45,46 +44,34 @@ def ensure_correct_user(fn):
         return abort(401, "Unauthorized")
     return wrapper
 
+# CPed over code
 
-user_messages_fields = {
-    'id': fields.Integer,
-    'name': fields.String,
-    'created': fields.DateTime(dt_format='rfc822'),
-}
+# def ensure_correct_user(fn):
+#     @wraps(fn)
+#     def wrapper(*args, **kwargs):
+#         if kwargs.get('id') != current_user.id:
+#             flash({'text': "Not Authorized", 'status': 'danger'})
+#             return redirect(url_for('root'))
+#         return fn(*args, **kwargs)
+#     return wrapper
+
+users_blueprint = Blueprint(
+  'users',
+  __name__,
+  template_folder='templates'
+)
+
+users_api = Api(Blueprint('users_api', __name__))
 
 user_fields= {
     'id': fields.Integer,
     'username': fields.String,
-    'messages': fields.Nested(user_messages_fields),
 }
-
-token_fields = {
-    'token': fields.String,
-    'id': fields.Integer
-}
-
-@users_api.resource('/users/auth')
-class authAPI(Resource):
-
-    @marshal_with(token_fields)
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', type=str, help='username')
-        parser.add_argument('password', type=str, help='password')
-        args = parser.parse_args()
-        token = authenticate(args['username'], args['password'])
-        if token:
-            found_user = User.query.filter_by(username= args['username']).first()
-            obj = {'token': token, 'id': found_user.id}
-            # this looks like where the JWT token is being returned,
-            # and specified to have an id element
-            return obj
-        return abort(400, "Invalid Credentials")
 
 @users_api.resource('/users')
 class usersAPI(Resource):
 
-    @jwt_required
+    # @jwt_required
     @marshal_with(user_fields)
     def get(self):
         return User.query.all()
@@ -96,43 +83,119 @@ class usersAPI(Resource):
         parser.add_argument('username', type=str, help='username')
         parser.add_argument('password', type=str, help='password')
         args = parser.parse_args()
+
         try:
             new_user = User(args['username'], args['password'])
             db.session.add(new_user)
             db.session.commit()
         except IntegrityError as e:
-            return "Username already exists"
+            return "Username for API already exists"
         return new_user
 
-@users_api.resource('/users/<int:id>')
-class UserAPI(Resource):
 
-    @jwt_required
-    @ensure_correct_user
-    @marshal_with(user_fields)
-    def get(self, id):
-        return User.query.get_or_404(id)
 
-    @jwt_required
-    @ensure_correct_user
-    @marshal_with(user_fields)
-    def put(self, id):
-        found_user = User.query.get_or_404(id)
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', type=str, help='username')
-        parser.add_argument('password', type=str, help='password')
-        args = parser.parse_args()
-        found_user.name = args['username']
-        found_user.password = bcrypt.generate_password_hash(args['password']).decode('UTF-8')
+@users_blueprint.route('/', methods=["GET"])
+def index():
+  search = request.args.get('q')
+  users = None
+  if search is None or search == '':
+    users = User.query.all()
+  else:
+    users = User.query.filter(User.username.like("%%%s%%" % search)).all()
+  return render_template('users/index.html', users=users )
+
+@users_blueprint.route('/signup', methods=["GET", "POST"])
+def signup():
+  form = UserForm()
+  if request.method == "POST":
+    if form.validate():
+      try:
+        new_user = User(
+          username=form.username.data,
+          email=form.email.data,
+          password=form.password.data
+        )
+        if form.image_url.data:
+          new_user.image_url = form.image_url.data
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+      except IntegrityError as e:
+        flash({'text': "Username already taken", 'status': 'danger'})
+        return render_template('users/signup.html', form=form)
+      return redirect(url_for('root'))
+  return render_template('users/signup.html', form=form)
+
+@users_blueprint.route('/login', methods=["GET", "POST"])
+def login():
+  form = LoginForm()
+  if request.method == "POST":
+    if form.validate():
+      found_user = User.query.filter_by(username = form.username.data).first()
+      if found_user:
+        is_authenticated = bcrypt.check_password_hash(found_user.password, form.password.data)
+        if is_authenticated:
+          login_user(found_user)
+          flash({'text': "Hello, {}!".format(found_user.username), 'status': 'success'})
+          return redirect(url_for('root'))
+      flash({'text': "Invalid credentials.", 'status': 'danger'})
+      return render_template('users/login.html', form=form)
+  return render_template('users/login.html', form=form)
+
+@users_blueprint.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  flash({ 'text': "You have successfully logged out.", 'status': 'success' })
+  return redirect(url_for('users.login'))
+
+@users_blueprint.route('/<int:id>/edit')
+@login_required
+@ensure_correct_user
+def edit(id):
+  return render_template('users/edit.html', form=UserForm(), user=User.query.get(id))
+
+@users_blueprint.route('/<int:follower_id>/follower', methods=['POST', 'DELETE'])
+@login_required
+def follower(follower_id):
+  followed = User.query.get(follower_id)
+  if request.method == 'POST':
+    current_user.following.append(followed)
+  else:
+    current_user.following.remove(followed)
+  db.session.add(current_user)
+  db.session.commit()
+  return redirect(url_for('users.following', id=current_user.id))
+
+@users_blueprint.route('/<int:id>/following', methods=['GET'])
+@login_required
+def following(id):
+  return render_template('users/following.html', user=User.query.get(id))
+
+@users_blueprint.route('/<int:id>/followers', methods=['GET'])
+@login_required
+def followers(id):
+  return render_template('users/followers.html', user=User.query.get(id))  
+
+@users_blueprint.route('/<int:id>', methods =["GET", "PATCH", "DELETE"])
+def show(id):
+  found_user = User.query.get(id)
+  if request.method == 'GET' or current_user.is_anonymous or current_user.get_id() != str(id):
+    return render_template('users/show.html', user=found_user)
+  if request.method == b"PATCH":
+    form = UserForm(request.form)
+    if form.validate():
+      if bcrypt.check_password_hash(found_user.password, form.password.data):
+        found_user.username = form.username.data
+        found_user.email = form.email.data
+        found_user.image_url = form.image_url.data or None
         db.session.add(found_user)
         db.session.commit()
-
-        return found_user
-
-    @jwt_required
-    @ensure_correct_user
-    def delete(self, id):
-        user = User.query.get_or_404(id)
-        db.session.delete(user)
-        db.session.commit()
-        return None, 204
+        return redirect(url_for('users.show', id=id))
+      flash({ 'text': "Wrong password, please try again.", 'status': 'danger'})
+    return render_template('users/edit.html', form=form, user=found_user)
+  if request.method == b"DELETE":
+    db.session.delete(found_user)
+    db.session.commit()
+    return redirect(url_for('users.signup'))
+  
