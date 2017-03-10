1d5c23365a52edbb578c80fd35e9c955bcc10a70
Aaron M <aaron.m.manley@gmail.com>
2017-03-10 14:23:06 -0800

    added back auth for GET

--- project/users/views.py
+++ project/users/views.py
@@ -1,9 +1,11 @@
-from flask import redirect, request, url_for, Blueprint
+from flask import redirect, request, url_for, Blueprint, abort
 from project.models import User
 from project import db, bcrypt
 from sqlalchemy.exc import IntegrityError
 from functools import wraps
 from flask_restful import Api, Resource, reqparse, marshal_with, fields
+import jwt
+from jwt.exceptions import DecodeError
 
 # cped over code
 def authenticate(username, password):
@@ -15,6 +17,7 @@ def authenticate(username, password):
 def jwt_required(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
+        from IPython import embed; embed()
         if request.headers.get('authorization'):
             split_token = request.headers.get('authorization').split(' ')[1]
         try:
@@ -22,10 +25,10 @@ def jwt_required(fn):
             if token:
                 return fn(*args, **kwargs)
         except DecodeError as e:
-            return abort(401, "Please log in again")
+            return abort(401, "DecodeError, Please log in again")
         except UnboundLocalError as e:
-            return abort(401, "Please log in again")
-        return abort(401, "Please log in")
+            return abort(401, "UnboundLocalError, Please log in again")
+        return abort(401, "Something bad happened, Please log in")
     return wrapper
 
 def ensure_correct_user(fn):
@@ -68,7 +71,7 @@ user_fields= {
 @users_api.resource('/users')
 class usersAPI(Resource):
 
-    # @jwt_required
+    @jwt_required
     @marshal_with(user_fields)
     def get(self):
         return User.query.all()
